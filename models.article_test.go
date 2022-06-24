fd0cc16201703e4b4fb217bde84e4c261db88b26
DiegoAndriano <andrianodna@gmail.com>
2022-06-24 10:27:17 -0300

    wip

---
+++ models.article_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestGetAllArticles(t *testing.T) {
+  alist := getAllArticles()
+
+  if len(alist) != len(articleList) {
+    t.Fail()
+  }
+
+  // Check that each member is identical
+  for i, v := range alist {
+    if v.Content != articleList[i].Content ||
+      v.ID != articleList[i].ID ||
+      v.Title != articleList[i].Title {
+
+      t.Fail()
+      break
+    }
+  }
+}
